71725ecf29bbb2b80f8a9ad4cce400ad930b1d4e
Hassan Bulega <hassanbulega@gmail.com>
2023-08-23 22:43:39 +0300

    add constants

---
+++ app/constants/index.js
@@ -0,0 +1,78 @@
+import FBLogo from '../assets/icon-facebook.svg'
+import YTLogo from "../assets/icon-youtube.svg";
+import TWLogo from '../assets/icon-twitter.svg'
+import PINLogo from "../assets/icon-pinterest.svg";
+import INSLogo from "../assets/icon-instagram.svg";
+export  const links = [
+  {
+    id: "1",
+    name: "Home",
+    href: '/'
+  },
+  {
+    id: "2",
+    name: "Flowers",
+    href: '/flowers'
+  },
+  {
+    id: "3",
+    name: "Plants",
+    href: '/plants'
+  },
+  {
+    id: "4",
+    name: "About Us",
+    href: '/about'
+  },
+  {
+    id: "5",
+    name: "Contact Us",
+    href: '/contact'
+  },
+];
+
+export const otherLinks = [
+  {
+    id: "1",
+    name: "Community",
+    href: "/",
+  },
+  {
+    id: "2",
+    name: "Events",
+    href: "/flowers",
+  },
+  {
+    id: "3",
+    name: "Privacy Policy",
+    href: "/plants",
+  },
+];
+
+export const logos = [
+  {
+    id: "1",
+    logo: FBLogo,
+    url: 'https://www.facebook.com'
+  },
+  {
+    id: "2",
+    logo: TWLogo,
+    url: 'https://www.twitter.com'
+  },
+  {
+    id: "3",
+    logo: INSLogo,
+    url: 'https://www.instagram.com'
+  },
+  {
+    id: "4",
+    logo: PINLogo,
+    url: 'https://www.pinterest.com'
+  },
+  {
+    id: "5",
+    logo: YTLogo,
+    url: 'https://www.youtube.com'
+  },
+];
