ec03f4c32048bebcfa79a563e843105e834e3e32
Hassan Bulega <hassanbulega@gmail.com>
2023-08-23 22:45:32 +0300

    feat: add Navbar and Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,83 @@
+import { links, otherLinks, logos } from "../constants";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../assets/logo-white.svg";
+import FBLogo from "../assets/icon-facebook.svg";
+import YTLogo from "../assets/icon-youtube.svg";
+import TWLogo from "../assets/icon-twitter.svg";
+import PINLogo from "../assets/icon-pinterest.svg";
+import INSLogo from "../assets/icon-instagram.svg";
+
+const Footer = () => {
+  return (
+    <footer className='bg-veryDarkBlue'>
+      <div className='container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0'>
+        <div className='flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start'>
+          <div className='mx-auto my-6 text-center text-white md:hidden'>
+            {" "}
+            Copyright &copy; 2023, All Rights Reserved{" "}
+          </div>
+          <div>
+            <Image src={Logo} className='h-8' alt='' />
+          </div>
+          <div className='flex justify-center space-x-4'>            
+            {logos.map((logo) => (
+              <Link
+                href={logo.url}
+                key={logo.id}
+                className='hover:text-brightRed'
+              >
+                <Image src={logo.logo} alt='' className='h-6' />
+              </Link>
+            ))}
+          </div>
+        </div>
+        {/* list container */}
+        <div className='flex justify-around space-x-32'>
+          <div className='flex flex-col space-y-3 text-white '>
+            {links.map((link) => (
+              <Link
+                href={link.href}
+                key={link.id}
+                className='hover:text-brightRed'
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+          <div className='flex flex-col space-y-3 text-white'>
+            {otherLinks.map((link) => (
+              <Link
+                href={link.href}
+                key={link.id}
+                className='hover:text-brightRed'
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+        {/* Input Container */}
+        <div className='flex flex-col justify-between'>
+          <form>
+            <div className='flex  space-x-3'>
+              <input
+                type='email'
+                className='flex-1 px-4 rounded-full focus:outline-none'
+                placeholder='Get updates with email'
+              />
+              <button className='px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none'>
+                Subscribe{" "}
+              </button>
+            </div>
+          </form>
+          <div className='hidden text-white md:block'>
+            {" "}
+            Copyright &copy; 2022, All Rights Reserved{" "}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import { links } from "../constants";
+import Image from "next/image";
+import Logo from "../assets/logo.svg";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav className='relative container mx-auto p-6'>
+      <div className='flex items-center justify-between'>
+        {/* Icon */}
+        <div className='pt-2'>
+          <Image src={Logo} alt='Becky Flowers Logo' width={200} />
+        </div>
+        {/* links */}
+        <div className='hidden space-x-6 md:flex'>
+          {links.map((link) => (
+            <Link
+              href={link.href}
+              key={link.id}
+              className='hover:text-darkGrayishBlue'
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
+        {/* Button */}
+        <Link
+          href='/flowers'
+          className='hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block'
+        >
+          Get Flowers
+        </Link>
+        {/* Hamburger */}
+        <button
+          id='menu-btn'
+          className='block hamburger md:hidden focus:outline-none'
+        >
+          <span className='hamburger-top'></span>
+          <span className='hamburger-middle'></span>
+          <span className='hamburger-bottom'></span>
+        </button>
+      </div>
+      {/* Mobile-Menu */}
+      <div className='md:hidden'>
+        <div
+          id='menu'
+          className='absolute flex-col items-center hidden self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md'
+        >
+          {links.map((link) => (
+            <Link href={link.href} key={link.id}>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
