962bef0adfa4e330bc99da4f49ca9b926a7fbaa1
Hassan Bulega <hassanbulega@gmail.com>
2023-08-30 10:24:26 +0300

    styles(css): add custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "19.5px"],
+      lg: ["18px", "21.94px"],
+      xl: ["20px", "24.38px"],
+      "2xl": ["24px", "29.26px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "8xl": ["87px", "100px"],
+    },
     extend: {
       colors: {
         brightRed: "hsl(12, 88%, 59%)",
@@ -16,6 +27,11 @@ module.exports = {
         veryDarkBlue: "hsl(233, 12%, 13%)",
         veryPaleRed: "hsl(13, 100%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
+        primary: "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)",
       },
     },
   },
