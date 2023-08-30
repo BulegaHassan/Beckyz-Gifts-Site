a81970b1a19ead69097856909c2fdba19cc37bb1
Hassan Bulega <hassanbulega@gmail.com>
2023-08-30 10:20:26 +0300

    feat: add components to main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import { Hero, PopularProducts, Testimonials, Services } from "./components";
 export default function Home() {
   return (
     <main>
-      <h1>Becky's Flowers</h1>
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <Testimonials />
+      </section>
     </main>
-  )
+  );
 }
