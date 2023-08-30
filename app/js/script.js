5748fc0c9f0af63e10cd36f32e61e73b53b51ba6
Hassan Bulega <hassanbulega@gmail.com>
2023-08-30 10:26:11 +0300

    perf: add script for toggling mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 if (typeof window !== "undefined") {
   const btn = document.getElementById("menu-btn");
   const nav = document.getElementById("menu");
 
   btn.addEventListener("click", () => {
-    console.log('clicked')
+    console.log("clicked");
     btn.classList.toggle("open");
     nav.classList.toggle("flex");
     nav.classList.toggle("hidden");
