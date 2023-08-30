6bc2ee7b70d9639594302c816f44c0fffc7f93a2
Hassan Bulega <hassanbulega@gmail.com>
2023-08-30 10:14:24 +0300

    feat: add services component

---
+++ app/components/Services.jsx
@@ -0,0 +1,22 @@
+import { services } from "../constants";
+import ServicesCard from "./ServicesCard";
+
+const Services = () => {
+  return (
+    <>
+      <section>
+        <h3 className='font-palanquin text-center text-4xl font-bold py-9'>
+          Why 
+          <span className='text-coral-red'> Choose</span>
+        {' '}  Us?
+        </h3>        
+      </section>
+      <section className='max-container flex justify-center flex-wrap gap-9'>
+        {services.map((service) => (
+          <ServicesCard key={service.label} {...service} />
+        ))}
+      </section>
+    </>
+  );
+};
+export default Services;
