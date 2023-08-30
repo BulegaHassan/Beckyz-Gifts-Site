857f7ce9926b0f7ef443da640b1e4bc183c1825c
Hassan Bulega <hassanbulega@gmail.com>
2023-08-30 10:18:08 +0300

    add all components in index file

--- app/components/index.js
+++ app/components/index.js
@@ -1,6 +1,22 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProductCard from "./PopularProductCard";
+import PopularProducts from "./PopularProducts";
+import Aboutsection from "./Aboutsection";
+import Testimonials from "./Testimonials";
+import Services from "./Services";
+import ServicesCard from "./ServicesCard";
+import TestimonialCard from "./TestimonialCard";
 
 export {
-    Navbar,Footer
-}
+  Navbar,
+  Footer,
+  Hero,
+  PopularProducts,
+  PopularProductCard,
+  Testimonials,
+  TestimonialCard,
+  Services,
+  ServicesCard
+};
