95e52c118f7a1229a71cee4127754a9956f30e86
Hassan Bulega <hassanbulega@gmail.com>
2023-08-31 10:46:29 +0300

    feat:add gifts page Also via intercepting routes, an individual product can be viewed View with modal and full page

---
+++ app/gifts/page.jsx
@@ -0,0 +1,19 @@
+import { Product2 } from "../components";
+async function GiftsPage() {
+  const res = await fetch("http://localhost:4000/gifts");
+  const products = await res.json();
+  // console.log(flowers);
+  return (
+    <main className='min-h-screen max-w-7xl mx-auto px-5 xl:px-0 mt-24'>
+      <section className='flex flex-col space-y-12 pb-44'>
+        <h1 className='text-5xl font-bold text-center'>BECKY GIFTS</h1>
+        <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+          {products.map((product) => (
+            <Product2 key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
+export default GiftsPage;
--- app/plants/page.jsx
+++
@@ -1,6 +0,0 @@
-const PlantsPage = () => {
-  return (
-    <div>Plants Page</div>
-  )
-}
-export default PlantsPage
