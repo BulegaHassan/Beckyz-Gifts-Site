a2d46d99988b53314914f6efb42ff662511f4b9e
Hassan Bulega <hassanbulega@gmail.com>
2023-08-31 10:27:54 +0300

    fix!: configure allwed image sources

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn3.1800flowers.com",
+      "cdn2.1800flowers.com",
+      "cdn1.1800flowers.com",
+      "cdn2.harryanddavid.com",
+      "cdn4.fruitbouquets.com",
+    ],
+  },
+};
 
 module.exports = nextConfig
+
