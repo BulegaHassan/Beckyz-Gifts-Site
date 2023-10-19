ed525deeb29304016409ffcbf1b0bf6109a6e222
Hassan Bulega <hassanbulega@gmail.com>
2023-10-19 09:28:11 +0300

    styles: add gradient

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Navbar,Footer } from './components'
-const inter = Inter({ subsets: ['latin'] })
-import Script from 'next/script'
-import script from './js/script'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Navbar, Footer } from "./components";
+const inter = Inter({ subsets: ["latin"] });
+import Script from "next/script";
+import script from "./js/script";
 export const metadata = {
-  title: 'Beckys flowers ',
-  description: 'flowers worth it',
-}
+  title: "Beckys flowers ",
+  description: "gifts worth it",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
+    <html lang='en'>
+      <body
+        className={inter.className}
+        className='bg-gradient-to-r from-rose-200 via-amber-200 to-rose-300'
+      >
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
         <Script src={script} />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -3,16 +3,16 @@ import { Hero, PopularProducts, Testimonials, Services } from "./components";
 export default function Home() {
   return (
     <main>
-      <section className='xl:padding-l wide:padding-r padding-b'>
+      <section className='xl:padding-l wide:padding-r padding-b '>
         <Hero />
       </section>
-      <section className='padding'>
+      <section className='padding bg-gradient-to-r from-amber-200 via-rose-200 to-amber-300'>
         <PopularProducts />
       </section>
       <section className='padding'>
         <Services />
       </section>
-      <section className='padding'>
+      <section className='padding bg-gradient-to-r from-amber-200 via-rose-200 to-amber-300'>
         <Testimonials />
       </section>
     </main>
